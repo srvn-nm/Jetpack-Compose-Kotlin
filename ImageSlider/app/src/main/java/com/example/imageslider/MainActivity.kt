63c8eb2bacff164b01adff61bfb90194dff73e69
sarvin nami <srvn0nm@gmail.com>
2023-07-08 14:21:43 +0330

    adding movie screen to the main activity

--- ImageSlider/app/src/main/java/com/example/imageslider/MainActivity.kt
+++ ImageSlider/app/src/main/java/com/example/imageslider/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.example.imageslider
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.imageslider.presentation.MovieScreen
 import com.example.imageslider.ui.theme.ImageSliderTheme
+import com.example.imageslider.viewModel.MainViewModel
+import com.google.accompanist.pager.ExperimentalPagerApi
 
+@ExperimentalPagerApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +23,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val viewModel by viewModels<MainViewModel>()
+                    MovieScreen(viewModel = viewModel)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ImageSliderTheme {
-        Greeting("Android")
     }
 }
