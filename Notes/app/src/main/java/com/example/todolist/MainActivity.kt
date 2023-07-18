148e9e603f7bcf89e77ca3e19e332789df3e11c5
sarvin nami <srvn0nm@gmail.com>
2023-07-18 10:57:18 +0330

    create the navhost of the project

--- Notes/app/src/main/java/com/example/todolist/MainActivity.kt
+++ Notes/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -4,6 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.todolist.db.Note
+import com.example.todolist.ui.MyNotes
+import com.example.todolist.ui.NoteItem
 import com.example.todolist.ui.theme.TodoListTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,5 +28,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun NoteSetUp() {
-
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "notes") {
+        composable("notes") {
+            MyNotes(navController)
+        }
+        composable(
+            "note/{noteId}",
+            arguments = listOf(navArgument("noteId") { type = NavType.ParcelableType(Note::class.java) })
+        ) { backStackEntry ->
+            backStackEntry.arguments?.getParcelable<Note>("noteId")?.let {
+                NoteItem(
+                    navController = navController,
+                    note = it
+                )
+            }
+        }
+        /*...*/
+    }
 }
