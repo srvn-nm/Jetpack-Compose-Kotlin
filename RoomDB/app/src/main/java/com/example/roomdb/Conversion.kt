e032548d1f1ea50bbc34769c78c53106296a1945
sarvin nami <srvn0nm@gmail.com>
2023-07-19 14:48:43 +0330

    add conversion data class

---
+++ RoomDB/app/src/main/java/com/example/roomdb/Conversion.kt
@@ -0,0 +1,9 @@
+package com.example.roomdb
+
+data class Conversion(
+    val id: Int,
+    val description: String,
+    val convertFrom: String,
+    val convertTp: String,
+    val multiplyBy: Double
+)
