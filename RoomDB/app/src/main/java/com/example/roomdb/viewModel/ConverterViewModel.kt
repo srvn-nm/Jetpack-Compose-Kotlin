a77ce56592da008dd4979164988744af4a061955
sarvin nami <srvn0nm@gmail.com>
2023-07-22 10:07:08 +0330

    update the view model

--- RoomDB/app/src/main/java/com/example/roomdb/viewModel/ConverterViewModel.kt
+++ RoomDB/app/src/main/java/com/example/roomdb/viewModel/ConverterViewModel.kt
@@ -1,9 +1,14 @@
 package com.example.roomdb.viewModel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.roomdb.data.Conversion
+import com.example.roomdb.data.ConversionResult
+import com.example.roomdb.data.ConverterRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class ConverterViewModel : ViewModel() {
+class ConverterViewModel(private val repository: ConverterRepository) : ViewModel() {
     fun getConversions() = listOf(
         Conversion(1, "Pounds to Kilograms", "lbs", "kg", 0.453592),
         Conversion(2, "Kilograms to Pounds", "kg", "lbs", 2.20462),
@@ -12,4 +17,24 @@ class ConverterViewModel : ViewModel() {
         Conversion(5, "Miles to Kilometers", "mi", "km", 1.60934),
         Conversion(6, "Kilometers to Miles", "km", "mi", 0.621371),
     )
+
+    val resultList = repository.getSavedResults()
+
+    fun addResult(message1 : String, message2 : String){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertResult(ConversionResult(0,message1,message2))
+        }
+    }
+
+    fun removeResult(item : ConversionResult){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.deleteResult(item)
+        }
+    }
+
+    fun clearAll(){
+        viewModelScope.launch (Dispatchers.IO){
+            repository.deleteAllResults()
+        }
+    }
 }
