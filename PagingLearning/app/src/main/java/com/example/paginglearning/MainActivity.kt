0aa87488e1322e1d3bf88c2866c24c686a6ffc0a
sarvin nami <srvn0nm@gmail.com>
2023-07-22 22:35:49 +0330

    design the user cards and using coil image

--- PagingLearning/app/src/main/java/com/example/paginglearning/MainActivity.kt
+++ PagingLearning/app/src/main/java/com/example/paginglearning/MainActivity.kt
@@ -4,21 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -29,6 +38,7 @@ import com.example.paginglearning.model.UserData
 import com.example.paginglearning.ui.theme.PagingLearningTheme
 import com.example.paginglearning.ui.theme.Purple40
 import com.example.paginglearning.viewModel.UserViewModel
+import com.google.accompanist.coil.rememberCoilPainter
 import kotlinx.coroutines.flow.Flow
 
 class MainActivity : ComponentActivity() {
@@ -81,14 +91,73 @@ fun UserListDetail(user: Flow<PagingData<UserData>>) {
         }
         Spacer(modifier = Modifier.height(10.dp))
         LazyColumn {
-            items(count = userListItem.itemCount){ item ->
-                item?.let { UserLists(it) }
+            items(count = userListItem.itemCount) { index ->
+                if (userListItem[index] != null) {
+                    userListItem[index]?.let {
+                        println(it.first_name)
+                        UserCardView(
+                            user = it
+                        )
+                    }
+                }
             }
         }
     }
 }
 
-    @Composable
-    fun UserLists(it: Int) {
-        TODO("Not yet implemented")
+@Composable
+fun UserCardView(user: UserData) {
+    Card(
+        modifier = Modifier
+            .padding(start = 10.dp, top = 5.dp, end = 10.dp, bottom = 5.dp)
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        shape = RoundedCornerShape(10.dp)
+
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(10.dp)
+                .clip(RoundedCornerShape(8.dp))
+        ) {
+            Surface(
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            ) {
+                //coil image
+                Image(
+                    painter = rememberCoilPainter(
+                        request = user.avatar,
+                        fadeIn = true
+                    ),
+                    contentDescription = "user image",
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(10.dp)),
+                    contentScale = ContentScale.Crop
+                )
+            }
+
+            Column(
+                modifier = Modifier
+                    .padding(5.dp)
+                    .align(Alignment.CenterVertically)
+            ) {
+                Text(
+                    text = "Name: " + user.first_name + " " + user.last_name,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = Color.Black
+                )
+
+                Text(
+                    text = "Email: " + user.email,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 14.sp,
+                    color = Color.Black,
+                    modifier = Modifier.padding(20.dp)
+                )
+            }
+        }
     }
+}
