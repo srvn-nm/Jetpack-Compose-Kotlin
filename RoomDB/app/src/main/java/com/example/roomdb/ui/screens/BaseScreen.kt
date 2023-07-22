4dd6671637add237b0f0bc869e6d13bdaa6dfac4
sarvin nami <srvn0nm@gmail.com>
2023-07-22 09:22:45 +0330

    create the base screen composable

--- RoomDB/app/src/main/java/com/example/roomdb/ui/screens/BaseScreen.kt
+++ RoomDB/app/src/main/java/com/example/roomdb/ui/screens/BaseScreen.kt
@@ -1,12 +1,44 @@
 package com.example.roomdb.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.roomdb.viewModel.ConverterViewModel
 
 @Composable
 fun BaseScreen(
     modifier: Modifier = Modifier,
-
+    factory: ConverterViewModelFactory,
+    converterViewModel: ConverterViewModel = viewModel(factory = factory)
 ) {
+    val list = converterViewModel.getConversions()
+    val historyList = converterViewModel.resultList.collectAsState(initial = emptyList())
 
+    Column (
+        modifier = modifier
+            .padding(30.dp)
+    ){
+        TopScreen(list){
+            message1, message2 ->
+            converterViewModel.addResult(message1, message2)
+        }
+        Spacer(
+            modifier = modifier
+                .height(20.dp)
+        )
+        HistoryScreen(
+            historyList,{item->
+                converterViewModel.removeResult(item)
+            },
+            {
+                converterViewModel.clearAll()
+            }
+        )
+    }
 }
