10108817da4bedc301551fbc5e9741a7ac8d5766
sarvin nami <srvn0nm@gmail.com>
2023-07-23 09:48:21 +0330

    add @inject constructor for using hilt in ConverterViewModelFactory

--- RoomDB/app/src/main/java/com/example/roomdb/viewModel/ConverterViewModelFactory.kt
+++ RoomDB/app/src/main/java/com/example/roomdb/viewModel/ConverterViewModelFactory.kt
@@ -3,7 +3,8 @@ package com.example.roomdb.viewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider.NewInstanceFactory
 import com.example.roomdb.data.ConverterRepository
+import javax.inject.Inject
 
-class ConverterViewModelFactory(private val repository: ConverterRepository) : NewInstanceFactory(){
+class ConverterViewModelFactory@Inject constructor(private val repository: ConverterRepository) : NewInstanceFactory(){
     override fun <T : ViewModel> create(modelClass: Class<T>): T = ConverterViewModel(repository) as T
 }
