f749f480e6016f88895d55dfc3d0799325cee1f4
sarvin nami <srvn0nm@gmail.com>
2023-07-23 12:15:48 +0330

    add LaunchEffect

--- SplashScreenAnimation/app/src/main/java/com/example/splashScreenAnimation/MainActivity.kt
+++ SplashScreenAnimation/app/src/main/java/com/example/splashScreenAnimation/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.splashScreenAnimation
 
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.example.splashScreenAnimation.ui.theme.SplashScreenAnimationTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,5 +31,24 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun SplashScreenAnimation() {
     val scale = remember{ Animatable(0f) }
+    
+    LaunchedEffect(key1 = true){
+        scale.animateTo(
+            targetValue = 0.5f,
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
+        delay(3000L)
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
 
+    }
 }
