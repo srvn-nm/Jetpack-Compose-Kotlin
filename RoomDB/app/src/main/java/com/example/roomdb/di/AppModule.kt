4a37e50725bffa1cc78fc9c13b944f6420119c2d
sarvin nami <srvn0nm@gmail.com>
2023-07-23 09:55:00 +0330

    add @InstallIn(SingletonComponent::class) to tell hilt that we want this module to be for which hilt component. in this case we want it to be application

--- RoomDB/app/src/main/java/com/example/roomdb/di/AppModule.kt
+++ RoomDB/app/src/main/java/com/example/roomdb/di/AppModule.kt
@@ -1,7 +1,10 @@
 package com.example.roomdb.di
 
 import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 
 @Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
 }
