39d1f3d1c2420dd9b48e689e2f892a970a53d2cd
sarvin nami <srvn0nm@gmail.com>
2023-07-23 10:13:41 +0330

    use @inject for factory and let hilt to handle it

--- RoomDB/app/src/main/java/com/example/roomdb/MainActivity.kt
+++ RoomDB/app/src/main/java/com/example/roomdb/MainActivity.kt
@@ -12,24 +12,29 @@ import com.example.roomdb.data.ConverterRepositoryImpl
 import com.example.roomdb.ui.screens.BaseScreen
 import com.example.roomdb.viewModel.ConverterViewModelFactory
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var factory: ConverterViewModelFactory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //without hilt
-        val dao = ConverterDatabase.getInstance(application).converterDAO
-        val repository = ConverterRepositoryImpl(dao)
-        val factory = ConverterViewModelFactory(repository)
+//        //without hilt
+//        val dao = ConverterDatabase.getInstance(application).converterDAO
+//        val repository = ConverterRepositoryImpl(dao)
+//        val factory = ConverterViewModelFactory(repository)
 
         setContent {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    BaseScreen(factory = factory)
-                }
+            // A surface container using the 'background' color from the theme
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                BaseScreen(factory = factory)
+            }
         }
     }
 }
