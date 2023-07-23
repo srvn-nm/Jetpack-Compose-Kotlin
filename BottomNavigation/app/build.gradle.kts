6c44a117d0f10c296f756ed6b4d2d995cac4ed4a
sarvin nami <srvn0nm@gmail.com>
2023-07-23 11:40:48 +0330

    add necessary dependencies for bottom nav graph

--- BottomNavigation/app/build.gradle.kts
+++ BottomNavigation/app/build.gradle.kts
@@ -32,17 +32,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.4.6"
     }
     packaging {
         resources {
@@ -72,4 +72,6 @@ dependencies {
     //navigation
     //noinspection UseTomlInstead
     implementation("androidx.navigation:navigation-compose:2.6.0")
+    //noinspection UseTomlInstead
+    implementation("androidx.compose.material:material:1.4.3")
 }
