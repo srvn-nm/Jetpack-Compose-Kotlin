c778cb3431df4995ae2a3516e34db6c39575ef95
sarvin nami <srvn0nm@gmail.com>
2023-07-23 11:48:41 +0330

    create a screen for our objects

---
+++ BottomNavigation/app/src/main/java/com/example/bottomnavigation/BottomScreenDetail.kt
@@ -0,0 +1,32 @@
+package com.example.bottomnavigation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+
+@Composable
+fun BottomHomeScreenDetail(text: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.h5,
+            color = Color.Green,
+            fontFamily = FontFamily.Monospace,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
