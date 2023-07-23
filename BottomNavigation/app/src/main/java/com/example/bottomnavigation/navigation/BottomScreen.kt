c81521757f8d71685d90318221d1140e1ba1eb6a
sarvin nami <srvn0nm@gmail.com>
2023-07-23 10:46:26 +0330

    create a sealed class and the list of it's objects for using navigation

---
+++ BottomNavigation/app/src/main/java/com/example/bottomnavigation/navigation/BottomScreen.kt
@@ -0,0 +1,29 @@
+package com.example.bottomnavigation.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BottomScreen(
+    val route: String,
+    val title: String,
+    val icon: ImageVector
+){
+    object Home: BottomScreen("Home", "Home", Icons.Filled.Home)
+    object Favourite: BottomScreen("Favourite", "Favourite",Icons.Filled.Favorite)
+    object Search: BottomScreen("Search", "Search", Icons.Filled.Search)
+    object Settings: BottomScreen("Settings", "Settings", Icons.Filled.Settings)
+    object User: BottomScreen("User", "User", Icons.Filled.Person)
+}
+
+val bottomNavigationItems = listOf(
+    BottomScreen.Home,
+    BottomScreen.Favourite,
+    BottomScreen.Search,
+    BottomScreen.Settings,
+    BottomScreen.User
+)
