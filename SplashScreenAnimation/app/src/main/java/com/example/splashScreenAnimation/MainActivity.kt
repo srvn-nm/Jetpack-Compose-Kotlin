ff02ccbcdb3d2c109a6271b81a28d8da18a0c427
sarvin nami <srvn0nm@gmail.com>
2023-07-23 12:26:14 +0330

    show an image successfully

--- SplashScreenAnimation/app/src/main/java/com/example/splashScreenAnimation/MainActivity.kt
+++ SplashScreenAnimation/app/src/main/java/com/example/splashScreenAnimation/MainActivity.kt
@@ -10,13 +10,16 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.unit.dp
 import com.example.splashScreenAnimation.ui.theme.SplashScreenAnimationTheme
 import kotlinx.coroutines.delay
 
@@ -33,9 +36,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SplashScreenAnimation() {
-    val scale = remember{ Animatable(0f) }
-    
-    LaunchedEffect(key1 = true){
+    val scale = remember { Animatable(0f) }
+
+    LaunchedEffect(key1 = true) {
         scale.animateTo(
             targetValue = 0.5f,
             animationSpec = tween(
@@ -54,7 +57,11 @@ fun SplashScreenAnimation() {
     ) {
         Image(
             painter = painterResource(id = R.drawable.jetpack_compose),
-            contentDescription = "splash image"
-            )
+            contentDescription = "splash image",
+            modifier = Modifier
+                .width(400.dp)
+                .height(400.dp)
+                .scale(scale.value)
+        )
     }
 }
