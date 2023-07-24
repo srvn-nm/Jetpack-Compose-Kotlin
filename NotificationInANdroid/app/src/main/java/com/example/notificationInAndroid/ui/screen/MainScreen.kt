2598b8e3e8c6457937ccec273c756184fff57ff1
sarvin nami <srvn0nm@gmail.com>
2023-07-24 09:57:54 +0330

    debug main screen

---
+++ NotificationInANdroid/app/src/main/java/com/example/notificationInAndroid/ui/screen/MainScreen.kt
@@ -0,0 +1,38 @@
+package com.example.notificationInAndroid.ui.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.notificationInAndroid.viewModel.MainViewModel
+
+@Composable
+fun MainScreen(mainViewModel: MainViewModel = hiltViewModel()) {
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { mainViewModel.showSimpleNotification(context) }) {
+            Text(text = "Simple Notification")
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+        Button(onClick = { mainViewModel.updateSimpleNotification(context) }) {
+            Text(text = "Update Notification")
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+        Button(onClick = mainViewModel::cancelSimpleNotification) {
+            Text(text = "Cancel Notification")
+        }
+    }
+}
