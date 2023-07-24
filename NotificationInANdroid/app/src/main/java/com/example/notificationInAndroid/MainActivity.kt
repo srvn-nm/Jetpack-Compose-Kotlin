00b9ea1d70025d589e467ad3109fdf1bf3f25dd5
sarvin nami <srvn0nm@gmail.com>
2023-07-24 09:57:44 +0330

    debug main activity

--- NotificationInANdroid/app/src/main/java/com/example/notificationInAndroid/MainActivity.kt
+++ NotificationInANdroid/app/src/main/java/com/example/notificationInAndroid/MainActivity.kt
@@ -3,13 +3,7 @@ package com.example.notificationInAndroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.notificationInAndroid.ui.screen.MainScreen
 import com.example.notificationInAndroid.ui.theme.NotificationInANdroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +11,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NotificationInANdroidTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NotificationInANdroidTheme {
-        Greeting("Android")
     }
 }
