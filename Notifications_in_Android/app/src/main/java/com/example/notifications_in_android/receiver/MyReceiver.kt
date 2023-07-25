f904a7370603f4c680ffaae6940e38afbd50a8d7
sarvin nami <srvn0nm@gmail.com>
2023-07-25 11:33:39 +0330

    create a receiver class for notification action button

---
+++ Notifications_in_Android/app/src/main/java/com/example/notifications_in_android/receiver/MyReceiver.kt
@@ -0,0 +1,15 @@
+package com.example.notifications_in_android.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val message = intent?.getStringExtra("MESSAGE")
+        if (message != null) {
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
