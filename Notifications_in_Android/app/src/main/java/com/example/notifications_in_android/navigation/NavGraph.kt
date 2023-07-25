e20e6bd4f61e54b6ec9b7b18fba8cc9fb54077d9
sarvin nami <srvn0nm@gmail.com>
2023-07-25 11:32:41 +0330

    create a navgraph composable function for navigating between screens and through notification

---
+++ Notifications_in_Android/app/src/main/java/com/example/notifications_in_android/navigation/NavGraph.kt
@@ -0,0 +1,42 @@
+package com.example.notifications_in_android.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
+
+const val MY_URI = "https://stevdza-san.com"
+const val MY_ARG = "message"
+
+@Composable
+fun SetUpNavGraph(
+    navHostController: NavHostController
+) {
+    NavHost(
+        navController = navHostController,
+        startDestination = Screen.Main.route
+    ) {
+        composable(route = Screen.Main.route) {
+            MainScreen(navController = navHostController)
+        }
+
+        composable(
+            route = Screen.Details.route,
+            arguments = listOf(
+                navArgument(MY_ARG) {
+                    type = NavType.StringType
+                }),
+            deepLinks = listOf(
+                navDeepLink {
+                    uriPattern = "$MY_URI/$MY_ARG={$MY_ARG}"
+                })
+        ) {
+            it.arguments?.getString(MY_ARG)?.let { message ->
+                DetailsScreen(message = message, navController = navHostController)
+            }
+        }
+    }
+}
