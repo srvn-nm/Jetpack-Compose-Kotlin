abd9ebac49c3d4c53561f49ddf1b96934b114d6c
sarvin nami <srvn0nm@gmail.com>
2023-07-25 12:41:36 +0330

    create new function for progress bar and new builder for it

--- Notifications_in_Android/app/src/main/java/com/example/notifications_in_android/viewModel/MainViewModel.kt
+++ Notifications_in_Android/app/src/main/java/com/example/notifications_in_android/viewModel/MainViewModel.kt
@@ -4,13 +4,21 @@ import android.annotation.SuppressLint
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.notifications_in_android.di.MainNotificationCompatBuilder
+import com.example.notifications_in_android.di.SecondNotificationCompatBuilder
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
+    @MainNotificationCompatBuilder
     private val notificationBuilder: NotificationCompat.Builder,
-    private val notificationManager: NotificationManagerCompat,
+    @SecondNotificationCompatBuilder
+    private val notificationBuilder2: NotificationCompat.Builder,
+    private val notificationManager: NotificationManagerCompat
 ) : ViewModel() {
 
 
@@ -30,5 +38,33 @@ class MainViewModel @Inject constructor(
 
     fun cancelSimpleNotification() {
         notificationManager.cancel(1)
+    }
+
+    @SuppressLint("MissingPermission")
+    fun showProgress() {
+        val max = 10
+        var progress = 0
+        viewModelScope.launch {
+            while (progress != max) {
+                delay(1000)
+                progress += 1
+                notificationManager.notify(
+                    3,
+                    notificationBuilder2
+                        .setContentTitle("Downloading")
+                        .setContentText("${progress}/${max}")
+                        .setProgress(max, progress, false).build()
+                )
+            }
+            notificationManager.notify(
+                3,
+                notificationBuilder
+                    .setContentTitle("Completed!")
+                    .setContentText("")
+                    .setContentIntent(null)
+                    .clearActions()
+                    .setProgress(0, 0, false).build()
+            )
+        }
     }
 }
