400c9bdb46143be9a59de0ed6acb47e3b865aa41
sarvin nami <srvn0nm@gmail.com>
2023-07-25 11:37:09 +0330

    create a view model for using notification handlers

---
+++ Notifications_in_Android/app/src/main/java/com/example/notifications_in_android/viewModel/MainViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.notifications_in_android.viewModel
+
+import android.annotation.SuppressLint
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val notificationBuilder: NotificationCompat.Builder,
+    private val notificationManager: NotificationManagerCompat,
+) : ViewModel() {
+
+
+    @SuppressLint("MissingPermission")
+    fun showSimpleNotification() {
+        notificationManager.notify(1, notificationBuilder.build())
+    }
+
+    @SuppressLint("MissingPermission")
+    fun updateSimpleNotification() {
+        notificationManager.notify(
+            1, notificationBuilder
+                .setContentTitle("NEW TITLE")
+                .build()
+        )
+    }
+
+    fun cancelSimpleNotification() {
+        notificationManager.cancel(1)
+    }
+}
