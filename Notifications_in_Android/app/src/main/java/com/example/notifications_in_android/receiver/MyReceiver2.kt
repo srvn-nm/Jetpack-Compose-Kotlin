96b88dfd83f1c73756ac148fb0aa6ca64baa215a
sarvin nami <srvn0nm@gmail.com>
2023-07-25 15:39:56 +0330

    create new receiver for new notification

---
+++ Notifications_in_Android/app/src/main/java/com/example/notifications_in_android/receiver/MyReceiver2.kt
@@ -0,0 +1,19 @@
+package com.example.notifications_in_android.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyReceiver2: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        try {
+            val message = intent?.getStringExtra("MESSAGE")
+            if (message != null) {
+                Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
