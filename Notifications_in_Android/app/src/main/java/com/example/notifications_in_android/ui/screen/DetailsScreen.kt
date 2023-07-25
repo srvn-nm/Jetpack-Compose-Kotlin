4aaf6b601d102b5c9da44af21ae6cb900765dfb7
sarvin nami <srvn0nm@gmail.com>
2023-07-25 11:34:49 +0330

    create a details screen for notification screen

---
+++ Notifications_in_Android/app/src/main/java/com/example/notifications_in_android/ui/screen/DetailsScreen.kt
@@ -0,0 +1,30 @@
+package com.example.notifications_in_android.ui.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.navigation.NavHostController
+
+@Composable
+fun DetailsScreen(message: String, navController: NavHostController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = message,
+            style = TextStyle(
+                fontSize = MaterialTheme.typography.headlineLarge.fontSize,
+                fontWeight = FontWeight.Bold
+            )
+        )
+    }
+}
