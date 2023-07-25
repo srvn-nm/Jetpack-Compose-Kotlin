c2b5397fdb60b2ddab614af162ff113f172bd9d6
sarvin nami <srvn0nm@gmail.com>
2023-07-25 11:24:28 +0330

    update main Activity due to needed functionality

--- Notifications_in_Android/app/src/main/java/com/example/notifications_in_android/MainActivity.kt
+++ Notifications_in_Android/app/src/main/java/com/example/notifications_in_android/MainActivity.kt
@@ -3,44 +3,20 @@ package com.example.notifications_in_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.notifications_in_android.ui.theme.Notifications_in_AndroidTheme
+import androidx.navigation.compose.rememberNavController
+import com.example.notificationInAndroid.navigation.SetUpNavGraph
+import com.example.notificationInAndroid.ui.theme.NotificationInAndroidTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Notifications_in_AndroidTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            NotificationInAndroidTheme {
+                val navController = rememberNavController()
+                SetUpNavGraph(navController)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Notifications_in_AndroidTheme {
-        Greeting("Android")
     }
 }
