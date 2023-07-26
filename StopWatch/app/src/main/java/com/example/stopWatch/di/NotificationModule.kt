70dd1b976ebbbc843cb0cce0ac716f33d04ee254
sarvin nami <srvn0nm@gmail.com>
2023-07-26 16:20:57 +0330

    create an icon for NotificationModule

--- StopWatch/app/src/main/java/com/example/stopWatch/di/NotificationModule.kt
+++ StopWatch/app/src/main/java/com/example/stopWatch/di/NotificationModule.kt
@@ -25,7 +25,7 @@ object NotificationModule {
         return NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
             .setContentTitle("Stopwatch")
             .setContentText("00:00:00")
-            .setSmallIcon(R.drawable.ic_baseline_timer_24)
+            .setSmallIcon(R.drawable.baseline_timer_24)
             .setOngoing(true)
             .addAction(0, "Stop", ServiceHelper.stopPendingIntent(context))
             .addAction(0, "Cancel", ServiceHelper.cancelPendingIntent(context))
