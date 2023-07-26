973031f454dfcd9cb5f640742cea4cee2a2a1a4e
sarvin nami <srvn0nm@gmail.com>
2023-07-26 16:10:02 +0330

    necessary dependencies

--- StopWatch/app/build.gradle.kts
+++ StopWatch/app/build.gradle.kts
@@ -2,6 +2,8 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -11,6 +13,7 @@ android {
     defaultConfig {
         applicationId = "com.example.stopWatch"
         minSdk = 24
+        //noinspection OldTargetApi
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -31,17 +34,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.4.7"
     }
     packaging {
         resources {
@@ -67,4 +70,32 @@ dependencies {
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
+
+    //Dagger-Hilt
+    //noinspection UseTomlInstead
+    implementation("com.google.dagger:hilt-android:2.47")
+    //noinspection UseTomlInstead
+    kapt("com.google.dagger:hilt-android-compiler:2.47")
+    //noinspection UseTomlInstead
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0-alpha01")
+    //noinspection UseTomlInstead
+    implementation("androidx.savedstate:savedstate-ktx:1.2.1")
+    //this line fixed the apps keeps crashing
+    //noinspection UseTomlInstead
+    implementation ("androidx.navigation:navigation-compose:2.6.0")
+
+    //viewModel
+    //noinspection UseTomlInstead
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+
+    //noinspection UseTomlInstead
+    implementation("androidx.annotation:annotation:1.6.0")
+    //noinspection UseTomlInstead
+    implementation("androidx.localbroadcastmanager:localbroadcastmanager:1.1.0")
+
+    //icons
+    //noinspection UseTomlInstead
+    implementation ("androidx.compose.material:material-icons-core:1.4.3")
+    //noinspection UseTomlInstead
+    implementation ("androidx.compose.material:material-icons-extended:1.4.3")
 }
