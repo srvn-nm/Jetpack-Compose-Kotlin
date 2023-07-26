43be1c342a3560b75198e6c0b1e7419e2dcc024d
sarvin nami <srvn0nm@gmail.com>
2023-07-26 17:00:47 +0330

    add necessary changes to manifest

--- StopWatch/app/src/main/java/com/example/stopWatch/ui/activity/MainActivity.kt
+++ StopWatch/app/src/main/java/com/example/stopWatch/ui/activity/MainActivity.kt
@@ -1,27 +1,22 @@
 package com.example.stopWatch.ui.activity
 
+import android.Manifest
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
+import android.os.Build
 import android.os.Bundle
 import android.os.IBinder
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.stopWatch.Manifest
 import com.example.stopWatch.service.StopwatchService
 import com.example.stopWatch.ui.screens.MainScreen
 import com.example.stopWatch.ui.theme.StopWatchTheme
@@ -52,6 +47,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
