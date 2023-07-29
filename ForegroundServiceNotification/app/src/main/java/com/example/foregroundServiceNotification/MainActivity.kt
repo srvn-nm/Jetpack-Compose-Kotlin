2021ae82582eca602936a65a3cd85abc45f29b71
sarvin nami <srvn0nm@gmail.com>
2023-07-29 15:39:33 +0330

    update main activity

--- ForegroundServiceNotification/app/src/main/java/com/example/foregroundServiceNotification/MainActivity.kt
+++ ForegroundServiceNotification/app/src/main/java/com/example/foregroundServiceNotification/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MakeForegroundService()
                 }
             }
         }
@@ -30,17 +30,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MakeForegroundService() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ForegroundServiceNotificationTheme {
-        Greeting("Android")
-    }
 }
