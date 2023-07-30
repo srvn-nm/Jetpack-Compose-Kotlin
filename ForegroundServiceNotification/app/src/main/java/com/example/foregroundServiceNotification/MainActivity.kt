52be743c5368bf7a8f2d580821517da716adcfdd
sarvin nami <srvn0nm@gmail.com>
2023-07-30 09:14:43 +0330

    write the main Activity

--- ForegroundServiceNotification/app/src/main/java/com/example/foregroundServiceNotification/MainActivity.kt
+++ ForegroundServiceNotification/app/src/main/java/com/example/foregroundServiceNotification/MainActivity.kt
@@ -3,14 +3,31 @@ package com.example.foregroundServiceNotification
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.foregroundServiceNotification.ui.theme.ForegroundServiceNotificationTheme
+import com.example.foregroundServiceNotification.ui.theme.Purple80
+import com.example.foregroundServiceNotification.utils.foregroundStartService
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,14 +39,60 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MakeForegroundService()
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Purple80)
+                            .padding(15.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = "Foreground Service",
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 20.sp
+                        )
+                    }
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Button(
+                            onClick = {
+                                foregroundStartService("Start")
+                            },
+                            modifier = Modifier
+                                .width(100.dp)
+                                .height(45.dp)
+                                .clip(RoundedCornerShape(10.dp))
+                        ) {
+                            Text(
+                                text = "Trigger",
+                                color = Color.White,
+                                fontSize = 15.sp
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(50.dp))
+                        Button(
+                            onClick = {
+                                foregroundStartService("Exit")
+                            },
+                            modifier = Modifier
+                                .width(100.dp)
+                                .height(45.dp)
+                                .clip(RoundedCornerShape(10.dp))
+                        ) {
+                            Text(
+                                text = "Exit",
+                                color = Color.White,
+                                fontSize = 15.sp
+                            )
+                        }
+                    }
                 }
             }
         }
     }
-}
-
-@Composable
-fun MakeForegroundService() {
-
 }
