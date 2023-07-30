415775440e92efecfd8b8f656ceb61bc815e4e77
sarvin nami <srvn0nm@gmail.com>
2023-07-30 09:17:20 +0330

    add necessary things to manifest

--- ForegroundServiceNotification/app/src/main/java/com/example/foregroundServiceNotification/MainActivity.kt
+++ ForegroundServiceNotification/app/src/main/java/com/example/foregroundServiceNotification/MainActivity.kt
@@ -17,7 +17,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
