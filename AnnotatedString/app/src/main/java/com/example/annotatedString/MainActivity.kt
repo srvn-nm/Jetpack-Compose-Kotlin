a9312959902a3fb63b21ee0e4cbbf42a6e9fcecc
sarvin nami <srvn0nm@gmail.com>
2023-08-01 09:26:02 +0330

    new project to learn annotated strings

---
+++ AnnotatedString/app/src/main/java/com/example/annotatedString/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.annotatedString
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.annotatedString.ui.theme.AnnotatedStringTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AnnotatedStringTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AnnotatedString("Hello. I am Sarvin. ^-^", "Sarvin")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AnnotatedString(text: String, annotatedPart: String) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+    AnnotatedStringTheme {
+        AnnotatedString("Hello. I am Sarvin. ^-^", "Sarvin")
+    }
+}
