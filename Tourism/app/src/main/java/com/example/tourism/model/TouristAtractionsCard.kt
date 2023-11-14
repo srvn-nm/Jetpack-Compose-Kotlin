ff78c2b7c5eb0d568357ea16a57eda69c37fdf5a
sarvin nami <srvn0nm@gmail.com>
2023-11-15 00:56:13 +0330

    beautify the design

--- Tourism/app/src/main/java/com/example/tourism/model/TouristAtractionsCard.kt
+++ Tourism/app/src/main/java/com/example/tourism/model/TouristAtractionsCard.kt
@@ -1,6 +1,7 @@
 package com.example.tourism.model
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,67 +9,76 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.tourism.ui.theme.md_theme_light_inversePrimary
 
 @Composable
 fun AttractionCard(attraction: Attraction) {
     Card(
         modifier = Modifier
             .wrapContentHeight()
-            .wrapContentWidth(),
-        shape = CircleShape,
-        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
+            .wrapContentWidth()
+            .background(MaterialTheme.colorScheme.primary)
+            .padding(20.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp),
     ) {
-        Row(
+        Column(
             modifier = Modifier
-                .border(0.25.dp, color = Color.Gray, shape = CircleShape)
-                .padding(10.dp)
-                .wrapContentHeight()
+                .padding(20.dp)
+                .border(0.25.dp, color = MaterialTheme.colorScheme.inversePrimary, shape = RoundedCornerShape(0.5.dp))
+                .wrapContentSize()
         ) {
-            Image(
-                painter = painterResource(id = attraction.image),
-                contentDescription = "Attraction image",
-                contentScale = ContentScale.Crop,
+            Row(
                 modifier = Modifier
-                    .size(64.dp)
-                    .clip(CircleShape)
-            )
-            Column {
+                    .padding(20.dp)
+                    .wrapContentHeight()
+
+            ) {
+                Image(
+                    painter = painterResource(id = attraction.image),
+                    contentDescription = "Attraction image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(128.dp)
+                        .clip(CircleShape)
+                        .shadow(5.dp)
+                )
                 Text(
                     text = "${attraction.name}\n",
-                    color = Color.DarkGray,
-                    fontSize = 20.sp,
-                    textAlign = TextAlign.Center,
-                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.bodyLarge,
                     modifier = Modifier
-                        .padding(start = 5.dp, top = 20.dp)
+                        .padding(start = 5.dp, top = 40.dp)
                         .fillMaxHeight()
-                )
-                Text(
-                    text = attraction.description,
-                    color = Color.DarkGray,
-                    fontSize = 16.sp,
-                    textAlign = TextAlign.Center,
-                    fontWeight = FontWeight.SemiBold,
-                    modifier = Modifier
-                        .padding(start = 5.dp, top = 20.dp)
-                        .fillMaxHeight()
+
                 )
             }
+            Text(
+                text = attraction.description,
+                style = MaterialTheme.typography.labelSmall,
+                textAlign = TextAlign.End,
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxHeight()
+            )
+
         }
     }
 }
