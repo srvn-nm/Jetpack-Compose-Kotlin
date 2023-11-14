759ec97640f3e612d5fad888ad85883b8d40f32b
sarvin nami <srvn0nm@gmail.com>
2023-11-15 01:00:15 +0330

    debugging

--- Tourism/app/src/main/java/com/example/tourism/model/TouristAtractionsCard.kt
+++ Tourism/app/src/main/java/com/example/tourism/model/TouristAtractionsCard.kt
@@ -23,11 +23,8 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.tourism.ui.theme.md_theme_light_inversePrimary
 
 @Composable
 fun AttractionCard(attraction: Attraction) {
