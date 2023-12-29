e0dfc241d550c5965da43b53daccd5f4b72e17c9
sarvin nami <srvn0nm@gmail.com>
2023-12-29 23:47:24 +0330

    refactor weather page show

--- WeatherApp/app/src/main/java/com/example/weatherApp/ui/screens/WeatherByName.kt
+++ WeatherApp/app/src/main/java/com/example/weatherApp/ui/screens/WeatherByName.kt
@@ -2,8 +2,10 @@ package com.example.weatherApp.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,9 +15,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -33,7 +35,7 @@ import kotlin.math.sqrt
 fun WeatherByNameScreen(navController: NavHostController, city: IranStates.IranCity?) {
     val viewModel = viewModel(modelClass = RequestViewModel::class.java)
     if (city != null) {
-        viewModel.cityWeatherApi(cityName = city.name)
+        viewModel.cityWeatherApi(city.latitude, city.longitude)
     }
     val cityWeather = viewModel.weatherInformation.collectAsState()
     Surface(
@@ -70,13 +72,16 @@ fun WeatherByNameScreen(navController: NavHostController, city: IranStates.IranC
                             color = Color.Black
                         )
                     )
-                    CardViewContent("Sunrise", "ic_sunrise2")
+                    PollutionShow(city.latitude, city.longitude)
+                    CardViewContent("Sunrise: ${cityWeather.value?.city?.sunrise}", R.drawable.ic_sunrise2, "Sunset: ${cityWeather.value?.city?.sunset}", R.drawable.ic_sunset2)
                     Spacer(modifier = Modifier.height(16.dp))
-                    CardViewContent("Temp", "ic_celcius")
-                    Spacer(modifier = Modifier.height(16.dp))
-                    CardViewContent("Temp Min", "ic_temperature_down")
-                    Spacer(modifier = Modifier.height(16.dp))
-                    CardViewContent("Pressure", "ic_temperature")
+                    cityWeather.value?.list?.forEach {
+                        CardViewContent("Temp: ${it.main.temp}", R.drawable.ic_celcius, "Feels Like: ${it.main.feels_like}", R.drawable.ic_temperature)
+                        Spacer(modifier = Modifier.height(16.dp))
+                        CardViewContent("Temp Min: ${it.main.temp_min}", R.drawable.ic_temperature_down, "Temp Max: ${it.main.temp_max}", R.drawable.ic_temperature_up)
+                        Spacer(modifier = Modifier.height(16.dp))
+                        CardViewContent("Pressure: ${it.main.pressure}", R.drawable.ic_temperature,"Humidity: ${it.main.humidity}", R.drawable.ic_humidity)
+                    }
                 }
             }
         }
@@ -84,7 +89,7 @@ fun WeatherByNameScreen(navController: NavHostController, city: IranStates.IranC
 }
 
 @Composable
-fun CardViewContent(text1: String, imageRes1: String, text2:String, imageRes2: String) {
+fun CardViewContent(text1: String, imageRes1: Int, text2:String, imageRes2: Int) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -111,13 +116,13 @@ fun CardViewContent(text1: String, imageRes1: String, text2:String, imageRes2: S
             }
             Column(modifier = Modifier.weight(1f)) {
                 Image(
-                    painter = painterResource(id = R.drawable.ic_sunrise2), // Replace with your drawable resource
+                    painter = painterResource(id = imageRes1), // Replace with your drawable resource
                     contentDescription = "Sunrise Icon",
                     modifier = Modifier.size(40.dp),
                     contentScale = ContentScale.Crop
                 )
                 Image(
-                    painter = painterResource(id = R.drawable.ic_sunrise2), // Replace with your drawable resource
+                    painter = painterResource(id = imageRes2), // Replace with your drawable resource
                     contentDescription = "Sunrise Icon",
                     modifier = Modifier.size(40.dp),
                     contentScale = ContentScale.Crop
@@ -149,6 +154,21 @@ fun PollutionShow(x: Double, y: Double) {
         in 201..300 -> Color(153,0,76)
         in 301..500 -> Color(76,0,38)
         else -> Color.Black
+    }
+
+    Card {
+        Row {
+            Box(
+                modifier = Modifier
+                    .size(10.dp)
+                    .background(
+                        color = color,
+                        shape = CircleShape
+                    )
+//                    .border(1.dp, Color.Black, CircleShape)
+            )
+            Text(text = "Pollution AQI Rate: ${rank.toString()}")
+        }
     }
 }
 
