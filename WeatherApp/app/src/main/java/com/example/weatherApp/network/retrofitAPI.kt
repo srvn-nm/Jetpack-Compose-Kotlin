8c733c8819f582141e3cf0e1a1b421a76a08bab6
sarvin nami <srvn0nm@gmail.com>
2023-12-30 02:35:23 +0330

    debug api calls queries

--- WeatherApp/app/src/main/java/com/example/weatherApp/network/retrofitAPI.kt
+++ WeatherApp/app/src/main/java/com/example/weatherApp/network/retrofitAPI.kt
@@ -3,12 +3,18 @@ package com.example.weatherApp.network
 import com.example.weatherApp.network.response.PollutionInfo
 import com.example.weatherApp.network.response.WeatherInfo
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface WeatherApiService {
 
-    @GET
-    suspend fun weatherPage(): WeatherInfo
+    @GET("forecast?")
+    suspend fun weatherPage(
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double,
+        @Query("appid") appid: String
+    ): WeatherInfo
 
-    @GET
+    @GET("LoadAQIMap?id=1")
     suspend fun pollutionInfo(): List<PollutionInfo>
 }
