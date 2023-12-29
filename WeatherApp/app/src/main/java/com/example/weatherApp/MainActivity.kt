ebbb28c0c2131b5d6a55a88c2f648ea0291fe7de
sarvin nami <srvn0nm@gmail.com>
2023-12-29 19:07:55 +0330

    create the first look of main activity

--- WeatherApp/app/src/main/java/com/example/weatherApp/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/example/weatherApp/MainActivity.kt
@@ -3,13 +3,12 @@ package com.example.weatherApp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.weatherApp.model.navigation.NavGraph
 import com.example.weatherApp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +16,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                val navController = rememberNavController()
+                Scaffold(
+                    topBar = {
+//                        ActionBar(scope = scope, scaffoldState = scaffoldState, title = "Idea Bank")
+                    },
+                    bottomBar = {
+//                        IdeaBankBottomNav(navController = navController)
+                    },
+                ) { padding ->
+                    Box(modifier = Modifier.padding(padding)) {
+                        NavGraph(navHostController = navController)
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeatherAppTheme {
-        Greeting("Android")
     }
 }
