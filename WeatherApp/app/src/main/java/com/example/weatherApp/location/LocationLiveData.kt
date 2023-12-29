0989667a510d6e30c0050ec0ffa0c7eec5d91f99
sarvin nami <srvn0nm@gmail.com>
2023-12-30 02:33:04 +0330

    new class for handling location live datas

---
+++ WeatherApp/app/src/main/java/com/example/weatherApp/location/LocationLiveData.kt
@@ -0,0 +1,7 @@
+package com.example.weatherApp.location
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+
+class LocationLiveData(var context: Context): LiveData<LocationDetails> {
+}
