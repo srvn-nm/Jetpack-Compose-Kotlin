f8781cf7b6d3ff399cdea41b4f378cff58e5fe26
sarvin nami <srvn0nm@gmail.com>
2023-12-29 23:52:50 +0330

    refactor first page show

--- WeatherApp/app/src/main/java/com/example/weatherApp/ui/screens/FirstPage.kt
+++ WeatherApp/app/src/main/java/com/example/weatherApp/ui/screens/FirstPage.kt
@@ -6,9 +6,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -19,6 +27,7 @@ import androidx.navigation.NavHostController
 import com.example.weatherApp.R
 import com.example.weatherApp.jsonHandler.Iran
 import com.example.weatherApp.jsonHandler.readJSONFromAssets
+import com.example.weatherApp.model.screen.WeatherScreens
 import com.google.gson.Gson
 
 @Composable
@@ -37,26 +46,44 @@ fun FirstPage(navController: NavHostController) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         item {
-            Image(
-                painter = painterResource(id = R.drawable.images),
-                contentDescription = null,
+            var nameShow by remember {
+                mutableStateOf(false)
+            }
+            Button(
+                onClick = { nameShow = !nameShow },
                 modifier = Modifier
+                    .padding(16.dp)
                     .fillMaxWidth()
-                    .height(200.dp)
-            )
-            Text("Please choose city name from below.")
-            DropdownStatesMenuBox(iranData.states, navController)
-//            Button(
-//                onClick = { navController.navigate(WeatherScreens.Name.passInfo(textValue)) },
-//                modifier = Modifier
-//                    .padding(16.dp)
-//                    .fillMaxWidth()
-//                    .height(61.dp),
-//                shape = MaterialTheme.shapes.medium,
-//                colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.DarkGray),
-//            ) {
-//                Text(text = "Weather Condition and Air Pollution.")
-//            }
+                    .height(61.dp),
+                shape = MaterialTheme.shapes.medium,
+                colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.DarkGray),
+            ) {
+                Text(text = "Weather Condition and Air Pollution by Choosing")
+            }
+            if (nameShow) {
+                Image(
+                    painter = painterResource(id = R.drawable.images),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                )
+                Text("Please choose city name from below.")
+                DropdownStatesMenuBox(iranData.states, navController)
+            }
+            if (!nameShow) {
+                Button(
+                    onClick = { navController.navigate(WeatherScreens.GPS.route) },
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth()
+                        .height(61.dp),
+                    shape = MaterialTheme.shapes.medium,
+                    colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.DarkGray),
+                ) {
+                    Text(text = "Weather Condition and Air Pollution using GPS")
+                }
+            }
         }
     }
 }
