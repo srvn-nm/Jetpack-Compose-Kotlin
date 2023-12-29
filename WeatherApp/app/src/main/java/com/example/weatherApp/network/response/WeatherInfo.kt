0e5125367f9a596c244576cdf1c94a7ec673c134
sarvin nami <srvn0nm@gmail.com>
2023-12-29 21:35:33 +0330

    refactoring

--- WeatherApp/app/src/main/java/com/example/weatherApp/network/response/WeatherInfo.kt
+++ WeatherApp/app/src/main/java/com/example/weatherApp/network/response/WeatherInfo.kt
@@ -14,42 +14,43 @@ data class WeatherInfo(
     val id: Int,
     val name: String,
     val cod: Int
-)
+) {
 
-data class Coord(
-    val lon: Double,
-    val lat: Double
-)
+    data class Coord(
+        val lon: Double,
+        val lat: Double
+    )
 
-data class Weather(
-    val id: Int,
-    val main: String,
-    val description: String,
-    val icon: String
-)
+    data class Weather(
+        val id: Int,
+        val main: String,
+        val description: String,
+        val icon: String
+    )
 
-data class Main(
-    val temp: Double,
-    val feels_like: Double,
-    val temp_min: Double,
-    val temp_max: Double,
-    val pressure: Int,
-    val humidity: Int
-)
+    data class Main(
+        val temp: Double,
+        val feels_like: Double,
+        val temp_min: Double,
+        val temp_max: Double,
+        val pressure: Int,
+        val humidity: Int
+    )
 
-data class Wind(
-    val speed: Double,
-    val deg: Int
-)
+    data class Wind(
+        val speed: Double,
+        val deg: Int
+    )
 
-data class Clouds(
-    val all: Int
-)
+    data class Clouds(
+        val all: Int
+    )
 
-data class Sys(
-    val type: Int,
-    val id: Int,
-    val country: String,
-    val sunrise: Long,
-    val sunset: Long
-)
+    data class Sys(
+        val type: Int,
+        val id: Int,
+        val country: String,
+        val sunrise: Long,
+        val sunset: Long
+    )
+}
