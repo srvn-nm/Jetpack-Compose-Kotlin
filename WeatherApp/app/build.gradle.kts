ce0a1e820d470815b2f80a41b94e8404f0e5e2fd
sarvin nami <srvn0nm@gmail.com>
2023-12-29 16:58:47 +0330

    add necessary dependencies

--- WeatherApp/app/build.gradle.kts
+++ WeatherApp/app/build.gradle.kts
@@ -87,4 +87,8 @@ dependencies {
     //navigation in compose
     //noinspection UseTomlInstead
     implementation ("androidx.navigation:navigation-compose:2.7.6")
+
+    //material
+    //noinspection UseTomlInstead
+    implementation ("com.google.android.material:material:1.11.0")
 }
