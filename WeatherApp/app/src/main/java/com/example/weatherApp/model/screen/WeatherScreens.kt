260731a558eb8521114c4447a435d47ef4e095fb
sarvin nami <srvn0nm@gmail.com>
2023-12-29 21:36:35 +0330

    refactor the screen

--- WeatherApp/app/src/main/java/com/example/weatherApp/model/screen/WeatherScreens.kt
+++ WeatherApp/app/src/main/java/com/example/weatherApp/model/screen/WeatherScreens.kt
@@ -1,11 +1,22 @@
 package com.example.weatherApp.model.screen
 
+import com.example.weatherApp.jsonHandler.IranStates
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import java.net.URLEncoder
+
 sealed class WeatherScreens(val route: String) {
     data object MainMenu : WeatherScreens(route = "first_page")
 
-    data object Name : WeatherScreens(route = "weather_by_name/{name}") {
-        fun passInfo(name: String): String {
-            return "weather_by_name/${name}"
+    data object Name : WeatherScreens(route = "weather_by_name/{city}") {
+        fun passInfo(city: IranStates.IranCity): String {
+            val moshi = Moshi.Builder()
+                .add(KotlinJsonAdapterFactory())
+                .build()
+            val jsonAdapter = moshi.adapter(IranStates.IranCity::class.java).lenient()
+            val cityJson = jsonAdapter.toJson(city)
+
+            return "weather_by_name/${cityJson}"
         }
     }
 
