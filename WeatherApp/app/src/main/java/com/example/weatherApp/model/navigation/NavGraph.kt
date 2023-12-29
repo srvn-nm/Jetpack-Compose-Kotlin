0df4a55b35c941c35e3ca76f6cb6c54143e331ce
sarvin nami <srvn0nm@gmail.com>
2023-12-29 21:34:53 +0330

    edit the nav graph

--- WeatherApp/app/src/main/java/com/example/weatherApp/model/navigation/NavGraph.kt
+++ WeatherApp/app/src/main/java/com/example/weatherApp/model/navigation/NavGraph.kt
@@ -2,14 +2,15 @@ package com.example.weatherApp.model.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
+import com.example.weatherApp.jsonHandler.IranStates
 import com.example.weatherApp.model.screen.WeatherScreens
 import com.example.weatherApp.ui.screens.FirstPage
 import com.example.weatherApp.ui.screens.WeatherByGPSScreen
 import com.example.weatherApp.ui.screens.WeatherByNameScreen
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 @Composable
 fun NavGraph(navHostController: NavHostController) {
@@ -25,16 +26,19 @@ fun NavGraph(navHostController: NavHostController) {
 
         composable(
             route = WeatherScreens.Name.route,
-            arguments = listOf(
-                navArgument("name") {
-                    type = NavType.StringType
-                }
-            )
-        ) {
-            WeatherByNameScreen(
-                navController = navHostController,
-                name = it.arguments!!.getString("name")!!
-            )
+        ) { backStackEntry ->
+            val cityJson = backStackEntry.arguments?.getString("city")
+            val moshi = Moshi.Builder()
+                .add(KotlinJsonAdapterFactory())
+                .build()
+            val jsonAdapter = moshi.adapter(IranStates.IranCity::class.java).lenient()
+            val city = cityJson?.let { jsonAdapter.fromJson(it) }
+            if (city != null) {
+                WeatherByNameScreen(
+                    navController = navHostController,
+                    city = city
+                )
+            }
         }
 
         composable(route = WeatherScreens.GPS.route) {
