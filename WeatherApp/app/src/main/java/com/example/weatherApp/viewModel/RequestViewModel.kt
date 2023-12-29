7fe4b1378bae75fcf71a9f1e910070064e38840d
sarvin nami <srvn0nm@gmail.com>
2023-12-29 23:07:03 +0330

    pollution api added in view model

--- WeatherApp/app/src/main/java/com/example/weatherApp/viewModel/RequestViewModel.kt
+++ WeatherApp/app/src/main/java/com/example/weatherApp/viewModel/RequestViewModel.kt
@@ -3,6 +3,7 @@ package com.example.weatherApp.viewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.weatherApp.network.WeatherApiService
+import com.example.weatherApp.network.response.PollutionInfo
 import com.example.weatherApp.network.response.WeatherInfo
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
@@ -17,6 +18,9 @@ class RequestViewModel : ViewModel() {
 
     private val _weatherInformation = MutableStateFlow<WeatherInfo?>(null)
     val weatherInformation: StateFlow<WeatherInfo?> get() = _weatherInformation
+
+    private val _pollutionInformation = MutableStateFlow(emptyList<PollutionInfo>())
+    val pollutionInformation: StateFlow<List<PollutionInfo>> get() = _pollutionInformation
 
     private val moshi: Moshi = Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
@@ -42,6 +46,29 @@ class RequestViewModel : ViewModel() {
                     .create(WeatherApiService::class.java)
 
                 _weatherInformation.value = retrofit.weatherPage()
+
+            } catch (exception: Exception) {
+                exception.printStackTrace()
+                if (exception.message.toString().contains("50")) {
+                    println("server time out")
+                } else {
+                    println("some things went wrong")
+                }
+            }
+        }
+    }
+
+    fun pollutionInformation(){
+        viewModelScope.launch {
+            try {
+                val retrofit = Retrofit.Builder()
+                    .baseUrl("https://aqms.doe.ir/Home/LoadAQIMap?id=1")
+                    .addConverterFactory(MoshiConverterFactory.create(moshi))
+                    .client(okHttpClient)
+                    .build()
+                    .create(WeatherApiService::class.java)
+
+                _pollutionInformation.value = retrofit.pollutionInfo()
 
             } catch (exception: Exception) {
                 exception.printStackTrace()
