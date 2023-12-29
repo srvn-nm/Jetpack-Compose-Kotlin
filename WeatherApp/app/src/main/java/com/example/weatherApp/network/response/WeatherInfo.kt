f7fd390dca01d5a354c2c32548097c76df1aff4f
sarvin nami <srvn0nm@gmail.com>
2023-12-29 23:47:54 +0330

    refactor weather info

--- WeatherApp/app/src/main/java/com/example/weatherApp/network/response/WeatherInfo.kt
+++ WeatherApp/app/src/main/java/com/example/weatherApp/network/response/WeatherInfo.kt
@@ -1,56 +1,71 @@
 package com.example.weatherApp.network.response
 
 data class WeatherInfo(
-    val coord: Coord,
-    val weather: List<Weather>,
-    val base: String,
-    val main: Main,
-    val visibility: Int,
-    val wind: Wind,
-    val clouds: Clouds,
-    val dt: Long,
-    val sys: Sys,
-    val timezone: Int,
-    val id: Int,
-    val name: String,
-    val cod: Int
-) {
+    val cod: Int,
+    val message: Int,
+    val cnt: Int,
+    val list: List<WeatherData>,
+    val city: City
+    ) {
 
-    data class Coord(
-        val lon: Double,
-        val lat: Double
+    data class City(
+        val id: Int,
+        val name: String,
+        val coord: Coord,
+        val country: String,
+        val population: Int,
+        val timezone: Int,
+        val sunrise: Long,
+        val sunset: Long
     )
 
-    data class Weather(
+    data class Coord(
+        val lat: Double,
+        val lon: Double
+    )
+
+    data class WeatherData(
+        val dt: Long,
+        val main: MainInfo,
+        val weather: List<WeatherInfo>,
+        val clouds: CloudInfo,
+        val wind: WindInfo,
+        val visibility: Int,
+        val pop: Double,
+        val sys: SysInfo,
+        val dt_txt: String
+    )
+
+    data class MainInfo(
+        val temp: Double,
+        val feels_like: Double,
+        val temp_min: Double,
+        val temp_max: Double,
+        val pressure: Int,
+        val sea_level: Int,
+        val grnd_level: Int,
+        val humidity: Int,
+        val temp_kf: Double
+    )
+
+    data class WeatherInfo(
         val id: Int,
         val main: String,
         val description: String,
         val icon: String
     )
 
-    data class Main(
-        val temp: Double,
-        val feels_like: Double,
-        val temp_min: Double,
-        val temp_max: Double,
-        val pressure: Int,
-        val humidity: Int
-    )
-
-    data class Wind(
-        val speed: Double,
-        val deg: Int
-    )
-
-    data class Clouds(
+    data class CloudInfo(
         val all: Int
     )
 
-    data class Sys(
-        val type: Int,
-        val id: Int,
-        val country: String,
-        val sunrise: Long,
-        val sunset: Long
+    data class WindInfo(
+        val speed: Double,
+        val deg: Int,
+        val gust: Double
+    )
+
+    data class SysInfo(
+        val pod: String
     )
 }
--- WeatherApp/app/src/main/java/com/example/weatherApp/ui/screens/WeatherByName.kt
+++ WeatherApp/app/src/main/java/com/example/weatherApp/ui/screens/WeatherByName.kt
@@ -2,8 +2,16 @@ package com.example.weatherApp.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
@@ -15,7 +23,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
