f8f24602b94da86b2802538ae1f44380c8dfec87
sarvin nami <srvn0nm@gmail.com>
2023-12-30 02:58:49 +0330

    location live data class finished

--- WeatherApp/app/src/main/java/com/example/weatherApp/location/LocationLiveData.kt
+++ WeatherApp/app/src/main/java/com/example/weatherApp/location/LocationLiveData.kt
@@ -1,8 +1,95 @@
 package com.example.weatherApp.location
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Looper
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.LiveData
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 
-class LocationLiveData(var context: Context): LiveData<LocationDetails>() {
+class LocationLiveData(private var context: Context) : LiveData<LocationDetails>() {
+    private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
 
+    override fun onActive() {
+        super.onActive()
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            location.also {
+                setLocationData(it)
+            }
+        }
+        startLocationUpdates()
+    }
+
+    private fun setLocationData(location: Location?) {
+        if (location != null) {
+            value = LocationDetails(location.longitude, location.latitude)
+        }
+    }
+
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+    }
+
+    companion object {
+        val ONE_MINUTE: Long = 60000
+        val locationRequest: LocationRequest = LocationRequest.create().apply {
+            interval = ONE_MINUTE
+            fastestInterval = ONE_MINUTE / 4
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+    }
+
+    private val locationCallback = object :LocationCallback(){
+        override fun onLocationResult(p0: LocationResult) {
+            super.onLocationResult(p0)
+            p0 ?: return
+            for (location in p0.locations){
+                setLocationData(location)
+            }
+        }
+    }
+
+    override fun onInactive() {
+        super.onInactive()
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
 }
