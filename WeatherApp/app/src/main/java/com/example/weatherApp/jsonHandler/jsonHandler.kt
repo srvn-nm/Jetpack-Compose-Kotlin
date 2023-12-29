978f944577b2ce231a903a953912ceeeb178b35b
sarvin nami <srvn0nm@gmail.com>
2023-12-30 02:32:30 +0330

    json reader debugging

--- WeatherApp/app/src/main/java/com/example/weatherApp/jsonHandler/jsonHandler.kt
+++ WeatherApp/app/src/main/java/com/example/weatherApp/jsonHandler/jsonHandler.kt
@@ -7,13 +7,26 @@ import java.io.InputStreamReader
 fun readJSONFromAssets(context: Context, path: String): String {
     return try {
         val file = context.assets.open(path)
+//        val size: Int = file.available()
+//        println(size)
+//        val buffer = ByteArray(size)
+//        println(buffer)
+//        file.read(buffer)
+//        file.close()
+//        println("meow")
+//        String(buffer, Charsets.UTF_8)
+        println(file)
         val bufferedReader = BufferedReader(InputStreamReader(file))
+        println(bufferedReader)
         val stringBuilder = StringBuilder()
+        println(stringBuilder)
         bufferedReader.useLines { lines ->
             lines.forEach {
                 stringBuilder.append(it)
             }
+            println(lines)
         }
+        println("meow")
         stringBuilder.toString()
     } catch (e: Exception) {
         e.printStackTrace()
