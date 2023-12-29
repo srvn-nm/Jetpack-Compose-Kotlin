ec47c9bac62c5ecb04e965d31c5497bda812b1fe
sarvin nami <srvn0nm@gmail.com>
2023-12-29 11:59:29 +0330

    create first page screen for choosing between name and gps

---
+++ WeatherApp/app/src/main/java/com/example/weatherApp/ui/screens/FirstPage.kt
@@ -0,0 +1,68 @@
+package com.example.weatherApp.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.weatherApp.R
+
+@Composable
+fun YourComposeScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.images),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+        )
+
+        var textValue by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            value = textValue,
+            onValueChange = { textValue = it },
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            label = { Text("Please type the city name here.") }
+        )
+
+        Button(
+            onClick = { /* TODO: Handle button click */ },
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .height(61.dp),
+            shape = MaterialTheme.shapes.medium,
+            colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.White),
+        ) {
+            Text(text = "Weather")
+        }
+    }
+}
